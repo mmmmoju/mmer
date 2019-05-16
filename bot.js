80393e36d5ff5bae9f08c3ea785fa54ee97a22d5
mmmmoju <50597545+mmmmoju@users.noreply.github.com>
2019-05-16 21:16:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("578156819613220866")
+let channel =     client.channels.get(578156819613220866)
 setInterval(function() {
-channel.send(`SPAM SPAM SPAM SPAM`);
+channel.send(SPAM SPAM SPAM SPAM);
 }, 30)
 })
 
