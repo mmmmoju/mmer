37ee15dd3b670616615824bbf35b6e7fe9191399
mmmmoju <50597545+mmmmoju@users.noreply.github.com>
2019-05-16 19:49:03 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("578156819613220866")
+setInterval(function() {
+channel.send(`SPAM SPAM SPAM SPAM`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
